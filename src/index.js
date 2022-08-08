2ca5aa31ccdcba12e66dc45c0bac2156977a033b
SuhaibAln <201710390@stu.meu.edu.jo>
2022-08-08 20:37:07 +0300

    lab11

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Profile from './profile';
+import About from './About';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-u5av3ltl.us.auth0.com"
+      clientId="Gxis1Yu66HvMsgj5DausCj0ckUYuzRJQ"
+      redirectUri={window.location.origin}
+    >
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </Router>
+    </Auth0Provider>,
   </React.StrictMode>
 );
---
+++ src/welcome.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+class Welcome extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Welcome to Your can of books</h1>
+                <p>
+                    Please login to view your books 
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Welcome;
